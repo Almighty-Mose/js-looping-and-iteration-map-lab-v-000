c7503c0755033d0af6cbab5ff1c6ed4890524bad
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-12 23:19:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function lowerCaseDrivers(list) {
 
 function nameToAttributes(list) {
   return list.map(function (name) {
-    return Object.assign({}, name, )
+    return Object.assign({}, name, {firstName: })
   });
 }
