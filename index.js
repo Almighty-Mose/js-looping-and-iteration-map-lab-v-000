298311109a6825595f7bf14d09bd36c2c8c95e41
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-12 23:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function nameToAttributes(list) {
 
 function attributesToPhrase(list) {
   return list.map(function (driver) {
-    return '${driver.name} is from ${driver.hometown}';
+    return `${driver.name} is from ${driver.hometown}`;
   });
 }
